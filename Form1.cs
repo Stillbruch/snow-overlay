5f5f88a748b890c117da37533098c0ccf7afdce9
rboe <alexandre.mai@bedag.ch>
2020-01-09 13:24:19 +0100

    new branch

--- Form1.cs
+++ Form1.cs
@@ -15,14 +15,12 @@ namespace SnowOverlay
     public partial class Form1 : Form
     {
         [DllImport("user32.dll")]
-        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
-        private static extern int GetWindowLongPtr(IntPtr hWnd, int nIndex);
+        private static extern IntPtr GetDC(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        private static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
 
-        private readonly Color transparencyColor = Color.Linen;
-        private readonly Size DEFAULT_SIZE = new Size(20, 20);
         private readonly Color DEFAULT_SNOWFLAKE_COLOR = Color.PaleTurquoise;
-        private const int DEFAULT_TIMER_INTERVAL = 50;
+        private const int DEFAULT_TIMER_INTERVAL = 100;
 
         private List<Snowflake> snowflake = new List<Snowflake>();
 
@@ -31,39 +29,31 @@ namespace SnowOverlay
         public Form1()
         {
             InitializeComponent();
-
-            BackColor = transparencyColor;
-            TransparencyKey = transparencyColor;
-            FormBorderStyle = FormBorderStyle.None;
-            TopMost = true;
-
             tmrTick.Interval = DEFAULT_TIMER_INTERVAL;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // make form click-through
-            SetWindowLong(Handle, -20, GetWindowLongPtr(Handle, -20) | 0x80000 | 0x20);
-            // other instructions
             tmrTick.Start();
-        }
-
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
-            snowflake.ForEach(s => e.Graphics.FillEllipse(new SolidBrush(DEFAULT_SNOWFLAKE_COLOR), new Rectangle(s.Location, s.Size)));            
         }
 
         private void TmrTick_Tick(object sender, EventArgs e)
         {
-            snowflake.Add(new Snowflake(new Point(rng.Next(0, Size.Width), 0), rng.Next(5, 20), rng.Next(1, 6)));
-            snowflake.ForEach(s => 
+            var desktopPtr = GetDC(IntPtr.Zero);
+            using (var g = Graphics.FromHdc(desktopPtr))
             {
-                if (s.Location.Y < Size.Height)
+                snowflake.Add(new Snowflake(new Point(rng.Next(0, Screen.PrimaryScreen.WorkingArea.Width), 0), rng.Next(5, 20), rng.Next(1, 6)));
+                snowflake.ForEach(s => g.FillEllipse(new SolidBrush(Color.Transparent), new Rectangle(s.Location, s.Size)));
+                snowflake.ForEach(s =>
                 {
-                    s.Fall();
-                }
-            });
-            Invalidate();
+                    if (s.Location.Y < Screen.PrimaryScreen.WorkingArea.Height)
+                    {
+                        s.Fall();
+                    }
+                });
+                snowflake.ForEach(s => g.FillEllipse(new SolidBrush(DEFAULT_SNOWFLAKE_COLOR), new Rectangle(s.Location, s.Size)));
+            }
+            ReleaseDC(IntPtr.Zero, desktopPtr);
         }
     }
 }
