69592915f50b5e6e212bacbad697e3206aa9e086
rboe <alexandre.mai@bedag.ch>
2020-01-09 12:08:51 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace SnowOverlay
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+        private static extern int GetWindowLongPtr(IntPtr hWnd, int nIndex);
+
+        private readonly Color transparencyColor = Color.Linen;
+        private readonly Size DEFAULT_SIZE = new Size(20, 20);
+        private readonly Color DEFAULT_SNOWFLAKE_COLOR = Color.PaleTurquoise;
+        private const int DEFAULT_TIMER_INTERVAL = 50;
+
+        private List<Snowflake> snowflake = new List<Snowflake>();
+
+        private readonly Random rng = new Random();
+               
+        public Form1()
+        {
+            InitializeComponent();
+
+            BackColor = transparencyColor;
+            TransparencyKey = transparencyColor;
+            FormBorderStyle = FormBorderStyle.None;
+            TopMost = true;
+
+            tmrTick.Interval = DEFAULT_TIMER_INTERVAL;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // make form click-through
+            SetWindowLong(Handle, -20, GetWindowLongPtr(Handle, -20) | 0x80000 | 0x20);
+            // other instructions
+            tmrTick.Start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            snowflake.ForEach(s => e.Graphics.FillEllipse(new SolidBrush(DEFAULT_SNOWFLAKE_COLOR), new Rectangle(s.Location, s.Size)));            
+        }
+
+        private void TmrTick_Tick(object sender, EventArgs e)
+        {
+            snowflake.Add(new Snowflake(new Point(rng.Next(0, Size.Width), 0), rng.Next(5, 20), rng.Next(1, 6)));
+            snowflake.ForEach(s => 
+            {
+                if (s.Location.Y < Size.Height)
+                {
+                    s.Fall();
+                }
+            });
+            Invalidate();
+        }
+    }
+}
---
+++ Snowflake.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SnowOverlay
+{
+    public class Snowflake
+    {
+        public Point Location { get; set; }
+        public Size Size { get; private set; }
+        public int Speed { get; private set; }
+
+        public Snowflake(Point location, int size, int speed = 1)
+        {
+            Location = location;
+            Size = new Size(size, size);
+            Speed = speed;
+        }
+
+        public void Fall()
+        {
+            Location = new Point(Location.X, Location.Y + Speed);
+        }
+    }
+}
